e2d91f32832c826ff4a6551a5ab5b0147ad42da3
Annalee <annaleeherrera@gmail.com>
2016-02-08 16:36:44 -0800

    sessions new login and root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :current_user
-  before_action :require_user
+  # before_action :require_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
       flash[:error] = "Please log in to view this section"
       redirect_to root_path
     end
+  end
+
+  def logged_in?
+  !current_user.nil?
   end
 end
