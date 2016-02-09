9529f81655d304182c807ebcdad4edbe3751c1c7
emgord <emgord@gmail.com>
2016-02-08 17:02:51 -0800

    fb omniauth working and creating new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,21 @@ class User < ActiveRecord::Base
   validates :email, :username, :uid, :provider, presence: true
 
   def self.find_or_create_from_omniauth(auth_hash)
-    # Find or create a user
-  end
-
-  def self.create_from_omniauth(auth_hash)
-    # Create a user
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      return user
+    elsif auth_hash["provider"] == "facebook"
+      user            = User.new
+      user.uid        = auth_hash["uid"]
+      user.provider   = auth_hash["provider"]
+      user.username   = auth_hash["info"]["name"]
+      user.email      = auth_hash["info"]["email"]
+      user.image      = auth_hash["info"]["image"]
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
   end
 end
