648bef0b5b4f681140cd786fe31052bf09e332ed
emgord <emgord@gmail.com>
2016-02-09 09:39:39 -0800

    require login working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :current_user
-  # before_action :require_user
+  before_action :require_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:error] = "Please log in to view this section"
-      redirect_to root_path
+      redirect_to login_path
     end
   end
 
