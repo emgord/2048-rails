02efcc6b56e4a011f9b2c0ad153adf479e46fa3a
Annalee <annaleeherrera@gmail.com>
2016-02-08 16:31:10 -0800

    rspec tests and login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
   root "site#index"
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
   get "/auth/:provider/callback", to:"sessions#create"
 end
