59d91d1d55af1033253b246888265819d3aa61f3
Annalee <annaleeherrera@gmail.com>
2016-02-09 16:59:47 -0800

    RemoteStorage getGame function building

--- public/js/remote_storage_manager.js
+++ public/js/remote_storage_manager.js
@@ -1,23 +1,23 @@
-window.fakeStorage = {
-  _data: {},
-
-  setItem: function (id, val) {
-    return this._data[id] = String(val);
-  },
-
-  getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
-  },
-
-  removeItem: function (id) {
-    return delete this._data[id];
-  },
-
-  clear: function () {
-    return this._data = {};
-  }
-};
-
+// window.fakeStorage = {
+//   _data: {},
+//
+//   setItem: function (id, val) {
+//     return this._data[id] = String(val);
+//   },
+//
+//   getItem: function (id) {
+//     return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+//   },
+//
+//   removeItem: function (id) {
+//     return delete this._data[id];
+//   },
+//
+//   clear: function () {
+//     return this._data = {};
+//   }
+// };
+//
 function RemoteStorageManager() {
   this.bestScoreKey     = "bestScore";
   this.gameStateKey     = "gameState";
@@ -41,18 +41,28 @@ RemoteStorageManager.prototype.localStorageSupported = function () {
 
 // Best score getters/setters
 RemoteStorageManager.prototype.getBestScore = function () {
-  return this.storage.getItem(this.bestScoreKey) || 0;
+  // return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
-RemoteStorageManager.prototype.setBestScore = function (score) {
-  this.storage.setItem(this.bestScoreKey, score);
+RemoteStorageManager.prototype.setBestScore = function(score){
+  // this.storage.setItem(this.bestScoreKey, score);
 };
 
 // Game state getters/setters and clearing
-RemoteStorageManager.prototype.getGameState = function () {
-  var stateJSON = this.storage.getItem(this.gameStateKey);
-  return stateJSON ? JSON.parse(stateJSON) : null;
-};
+RemoteStorageManager.prototype.getGameState = function(){
+  var url = "http://localhost:3000/gamesetups/1"
+  $.ajax(url, {
+    type: "GET"
+  })
+    .done(function(data){
+      console.log("success");
+      var stateJSON = data;
+      return stateJSON;
+    });
+  };
+
+  // var stateJSON = this.storage.getItem(this.gameStateKey);
+  // return stateJSON ? JSON.parse(stateJSON) : null;
 
 RemoteStorageManager.prototype.setGameState = function (gameState) {
   this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
