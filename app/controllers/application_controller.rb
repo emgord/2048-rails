a4e367b0296293becde1886c4e2afa34ec823aef
emgord <emgord@gmail.com>
2016-02-10 20:54:51 -0800

    added navbar and alert style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :current_user
-  before_action :require_user
+  # before_action :require_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
