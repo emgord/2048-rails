cef5abcf0afab21b568886b3657ca573a7aada3a
emgord <emgord@gmail.com>
2016-02-12 10:12:07 -0800

    removed unneeded load route from project

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,8 +3,4 @@ class SiteController < ApplicationController
   def index
   end
 
-  def load
-    @game_id = params[:id]
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   get "/auth/:provider/callback", to:"sessions#create"
   resources :gamesetups
-  get "gamesetups/load/:id" => "site#load", as:"load"
 end
